88422bde1a34542ad0a1d7be8aeab555c4ff7f39
phamvanbinhminh <phamvanbinhminh210@gmail.com>
2026-08-09 21:48:54 +0700

    update gpu flock

--- flock/src/main.cpp
+++ flock/src/main.cpp
@@ -1,22 +1,79 @@
 #include <emper/Emper_Engine.h>
-#include <Renderer.h>
+#include <OpenGLComputeBackend.h>
+#include <SDLOpenGLRenderer.h>
 #include <Flock.h>
 
-int main()
+#include <filesystem>
+
+int main(int argc, char** argv)
 {
+    if (argc > 0 && argv[0])
+        std::filesystem::current_path(
+            std::filesystem::absolute(argv[0]).parent_path());
+
     emper::Simulation simulation;
     simulation.initialize();
 
-    emper::backend::SDL3Renderer renderer("Emper Flock", 1280, 720);
+    emper::backend::OpenGLComputeBackend computeBackend;
+    emper::backend::SDLOpenGLRenderer renderer("Emper Flock", 1280, 720);
     simulation.setRenderer(&renderer);
 
     auto& world = simulation.world();
 
     emper::module::FlockConfig config;
-    config.mode = emper::interfaces::module::ComputeMode::CPU;
-    config.boidCount = 100'000;
+    config.mode = emper::interfaces::module::ComputeMode::GPU;
+    
+            /*
+            config.mode = emper::interfaces::module::ComputeMode::CPU;
+            CPUSetting
+            
+                config.boidCount = 100000;
 
-    emper::module::Flock flock(world, config);
+    config.worldWidth  = 1280.0f;
+    config.worldHeight = 720.0f;
+
+    config.maxSpeed     = 80.0f;
+    config.initialSpeed = 40.0f;
+    config.maxForce     = 25.0f;
+
+    config.perceptionRadius = 50.0f;
+    config.separationRadius = 30.0f;
+
+    config.separationWeight = 4.0f;
+    config.alignmentWeight  = 1.0f;
+    config.cohesionWeight   = 1.0f;
+
+    config.maxNeighbours = 64;
+    config.teamCount = 3;
+            
+            */
+
+
+            /*
+            
+            GPU setting
+            */
+    config.boidCount = 100000;
+
+    config.worldWidth  = 1280.0f;
+    config.worldHeight = 720.0f;
+
+    config.maxSpeed     = 80.0f;
+    config.initialSpeed = 40.0f;
+    config.maxForce     = 25.0f;
+
+    config.perceptionRadius = 50.0f;
+    config.separationRadius = 30.0f;
+
+    config.separationWeight = 4.0f;
+    config.alignmentWeight  = 1.0f;
+    config.cohesionWeight   = 1.5f;
+
+    config.maxNeighbours = 64;
+    config.teamCount = 3;
+
+
+    emper::module::Flock flock(world, config, &computeBackend);
     world.addSystem(&flock);
 
     simulation.start();
